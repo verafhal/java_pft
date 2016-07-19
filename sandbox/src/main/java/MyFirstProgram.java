cc8e630bfd07803ffde021923869ec6058d2ff92
verafhal <verafhal@gmail.com>
2016-07-19 20:48:31 +0300

    project dlya idea 15

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,9 +1,10 @@
-public class MyFirstProgram{
+public class MyFirstProgram {
 
-public static void main (String []args){
+    public static void main(String[] args) {
+       System.out.println ("Hello, word!");
 
-System.out.println("Hello,word!");
+    }
 
 
 }
-}
+
