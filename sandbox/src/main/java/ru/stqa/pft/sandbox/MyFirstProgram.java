68840e86ee7056f69226da8edcda3666bcf8c9e2
verafhal <verafhal@gmail.com>
2016-07-20 21:28:38 +0300

    izmeneni nastroyki formatirovaniya

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-       System.out.println ("Hello, word!");
+        System.out.println("Hello, word!");
 
     }
 
