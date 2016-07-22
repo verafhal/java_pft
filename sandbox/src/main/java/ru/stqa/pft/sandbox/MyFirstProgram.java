9f02283ad60ef86a08c31c9dd6d2122a0f84ce1d
verafhal <verafhal@gmail.com>
2016-07-22 14:49:30 +0300

    dobavleno neskolko funkziy

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,10 +3,27 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, word!");
+        hello("word");
+        hello("user");
+        hello("Vera");
+
+        double l = 5;
+
+        System.out.println("Ploshad kvadrata so storonoy " + l + "=" + area(l));
+        double a = 4;
+        double b = 6;
+        System.out.println("Ploshadc pryamougolnika so storonami " + a + " i " + b +"=" + area(a,b));
 
     }
+public static void hello(String somebody){
 
-
+    System.out.println("Hello,"+ somebody +"!");
+}
+public static double area(double len){
+   return len * len;
+}
+    public static double area (double a,double b){
+        return a*b;
+    }
 }
 
