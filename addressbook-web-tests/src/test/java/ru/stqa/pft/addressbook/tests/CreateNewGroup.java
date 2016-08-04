516a172e97a4c04bac67d3459fbe49cd9a7bb4ba
verafhal <verafhal@gmail.com>
2016-08-04 21:26:27 +0300

    Sozdanniy test ApplicationManager failed

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateNewGroup.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateNewGroup.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.tests;
 
 public class CreateNewGroup extends TestBase {
 
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+/**
+ * Created by Marom on 04/08/2016.
+ */
+public class TestBase {
+
+  private final ApplicationManager app = new ApplicationManager();
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    app.stop();
+  }
+
+}
