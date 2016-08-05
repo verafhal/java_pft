de88ed9141e2f62c01e2d2c2b3fb33f368fe2e77
verafhal <verafhal@gmail.com>
2016-08-05 17:37:09 +0300

    Sozdanie GroupDeletingTests +TestBase i vse testi unasledovani

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletingTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletingTests.java
@@ -1,3 +1,5 @@
+package ru.stqa.pft.addressbook;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -12,47 +14,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 import static org.openqa.selenium.OutputType.*;
 
-public class GroupDeletingTests {
-    FirefoxDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
+public class GroupDeletingTests extends TestBase {
+
     @Test
-    public void GroupDeletingTests() {
-        wd.get("http://localhost:8080/addressbook/");
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("קבוצות")).click();
-        if (!wd.findElement(By.name("selected[]")).isSelected()) {
-            wd.findElement(By.name("selected[]")).click();
-        }
-        wd.findElement(By.name("delete")).click();
-        wd.findElement(By.linkText("group page")).click();
-        wd.findElement(By.cssSelector("html")).click();
-        wd.findElement(By.cssSelector("html")).click();
+    public void testGroupDeleting() {
+      gotoGroupPage();
+      selectGroup();
+      deleteGroup();
+      returntoGroupPage();
+
+
     }
-    
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+
 }
