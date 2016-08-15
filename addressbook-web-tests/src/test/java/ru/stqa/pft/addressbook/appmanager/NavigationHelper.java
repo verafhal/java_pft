5bbfb3f7ba72b9636504498f990a2a9d695049a9
verafhal <verafhal@gmail.com>
2016-08-15 23:35:23 +0300

    optimizirovanni perehodi mejdu stranizami

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -14,10 +14,19 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
-   click(By.linkText("קבוצות"));
-  }
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
+      click(By.linkText("קבוצות"));
+    }
+
 
   public void gotoUserPage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("ההערה"));
   }
 }
